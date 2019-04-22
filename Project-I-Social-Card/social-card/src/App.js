76acb2ce9dd36de55ab6351adf96f22e5ef25434
alyssa-hatfield <47366310+xCthaeh@users.noreply.github.com>
2019-04-22 12:09:59 -0400

    imported components to app.js

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer'
 
 const App = () => {
   return (
